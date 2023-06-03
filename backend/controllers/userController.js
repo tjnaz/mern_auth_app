b1e88d0bb2790fa90fa52cbacc1a3eeeeaa1ab3d
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-03 18:07:09 +0530

    add: controller logics

---
+++ backend/controllers/userController.js
@@ -0,0 +1,8 @@
+// @desc    Auth User/set token
+// route    POST /api/users/auth
+// @access  Public
+const authUser = (req, res) => {
+  res.status(200).json({ message: "Auth User" });
+};
+
+export { authUser };
