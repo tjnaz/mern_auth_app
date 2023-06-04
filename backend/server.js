7fb0d413b92b1627b31d14b13a923f45fd90083a
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-04 16:49:49 +0530

    update: routes

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send(`Server is ready`));
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on port ${port}`));
