a33ac09965dbd050281fbeb1c633b9d58da45d7b
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-04 11:22:45 +0530

    add: async

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 // @desc    Auth User/set token
 // route    POST /api/users/auth
 // @access  Public
-const authUser = (req, res) => {
+const authUser = async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 };
 
