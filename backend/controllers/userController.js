d3964d0118088a1d18e80458ba59d2d04398f7c1
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-04 16:49:06 +0530

    refactor: code syntax readability

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,17 @@
 import asyncHandler from "express-async-handler";
 
-// @desc    Auth User/set token
-// route    POST /api/users/auth
-// @access  Public
-const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
-});
-
 // @desc    Register a new user
 // route    POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Register User" });
+});
+
+// @desc    Auth User/set token
+// route    POST /api/users/auth
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Auth User" });
 });
 
 // @desc    Logout user
