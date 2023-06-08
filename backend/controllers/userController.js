0667aa863ad57c02854d01de4f20d1cb93083f37
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-08 19:35:03 +0530

    feat: login the user after login cr. auth.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,23 +7,20 @@ import generateToken from "../utils/generateToken.js";
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
-
   const userExists = await User.findOne({ email });
-
-  if (userExists) {
-    res.status(400);
-    throw new Error("User already exists");
-  }
-
   const user = await User.create({
     name,
     email,
     password,
   });
 
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
   if (user) {
     generateToken(res, user._id);
-
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -40,10 +37,9 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-
   const user = await User.findOne({ email });
 
-  if (user) {
+  if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
